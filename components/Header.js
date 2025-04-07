c118c93ee4e24bef84bfcdf4f1573f6215a247e4
Patricks-Hub <83692732+dm1pfoy188@users.noreply.github.com>
2025-04-07 15:04:59 -0500

    Updated code for group project. Had to change image to get it to work properly for Michael. I have added components for employee card and header and added main page code all have external stylesheets

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>The Office EmployeeHub - Connect with Your Team</h1>
+    </header>
+  );
+};
+
+export default Header;
